7092dfa6ca50da03898f9304f3ef470b87a90247
Your Name <dariolopes2001@gmail.com>
2026-05-18 16:19:12 -0300

    desafio de caminho resolvido com recursao

--- leitura-escrita-arquivos/caminhos/desafio.py
+++ leitura-escrita-arquivos/caminhos/desafio.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 
-
 # def retornar_tamanho_dos_diretorios(caminho, profundidade=1):
 #     for arquivo in caminho.glob("*"):
 #         print(f"{arquivo.name} ---> {os.path.getsize(arquivo)}")
@@ -12,55 +11,91 @@ import os
 # retornar_tamanho_dos_diretorios(caminho, profundidade=1)
 
 
-visitados = set()
-tamanho = {}
-def pesquisar_diretorio(diretorio=(Path.home() / "Documents").glob("*")):
-    for arquivo in diretorio.glob("*"):
-        if arquivo.exists() and arquivo not in visitados:
-            print(f"--------Nome do arquivo: {arquivo.name} --> {os.path.getsize(arquivo)}")
-            if arquivo.is_file():
-                tamanho[arquivo] = os.path.getsize(arquivo)
-            if arquivo.is_dir() and any(arquivo.glob("*")):
-                return arquivo
-            else:
-                visitados.add(arquivo)
-                
-    visitados.add(arquivo.parent)
-    return arquivo.parent.parent  
+# Código para montar a hierarquia de pastas e arquivos
+def montar_arvore(pasta, hierarquia):
+    try:
+        lista_arquivos = os.listdir(pasta)
+    except PermissionError:
+        return    
+    if not hierarquia:
+        hierarquia.setdefault(pasta.name, {})
+    for arquivo in lista_arquivos:
+        arquivo = Path(Path.cwd() / pasta / arquivo)
+        nome_pasta = pasta.name
+        if arquivo.is_file():
+            nome_arquivo = arquivo.name
+            tamanho_arquivo = os.path.getsize(arquivo)
+            hierarquia.setdefault(nome_pasta, {}).setdefault(
+                nome_arquivo, {}
+            ).setdefault("tamanho", tamanho_arquivo)
+            hierarquia.setdefault(nome_pasta, {}).setdefault(
+                nome_arquivo, {}
+            ).setdefault("tipo", "arquivo")
+        elif arquivo.is_dir():
+            montar_arvore(
+                Path.cwd() / pasta / arquivo, hierarquia.setdefault(nome_pasta, {})
+            )
 
-def calcular_tamanho(tamanho, caminho_raiz):
-    tamanho_final = {}
-    for arquivo, tamanho_arquivo in tamanho.items():
-        while caminho_raiz in arquivo.parents or caminho_raiz == arquivo:
-            if arquivo.parent in tamanho_final or caminho_raiz == arquivo:
-                if arquivo != caminho_raiz:
-                    tamanho_final[arquivo.parent] += tamanho_arquivo 
-                else:
-                    tamanho_final[arquivo] += tamanho_arquivo                      
-            else:
-                if arquivo != caminho_raiz:
-                    tamanho_final[arquivo.parent] = tamanho_arquivo
-                else:
-                    tamanho_final[arquivo] = tamanho_arquivo 
-            arquivo = arquivo.parent      
-    return tamanho_final              
-    
-# for arquivo in (Path.home() / "Documents").glob("*"):
-for arquivo_pai in (Path("D:\\dario") / "Documents").glob("*"):
-    ainda_tem_arquivo = True
-    if arquivo_pai.exists():
-        print(f"Nome do arquivo: {arquivo_pai.name} -----> {os.path.getsize(arquivo_pai)}")
-        if arquivo_pai.is_dir() and any(arquivo_pai.glob("*")):
-            arquivo = arquivo_pai
-            while True:
-                if arquivo_pai.parent not in arquivo.parents:
-                    break 
-                if arquivo not in visitados:
-                    arquivo = pesquisar_diretorio(arquivo)
-                else:    
-                    arquivo = arquivo.parent    
-                    
-print("\n\n\n\n\n\n\n")                    
-tamanho_calculado = calcular_tamanho(tamanho,(Path("D:\\dario") / "Documents"))
-for key, value in tamanho_calculado.items():
-    print(f"Arquivo: {key} -----> Tamanho: {value/(1024*1024)}MB")
+
+# Código para Calcular tamanho das de pastas
+def calcular_tamanho(hierarquia):
+    soma_tamanho = 0
+
+    for nome, conteudo in hierarquia.items():
+        if nome == "tamanho":
+            continue
+        if conteudo.get("tipo") == "arquivo":
+            soma_tamanho += hierarquia[nome]["tamanho"]
+        else:
+            soma_tamanho += calcular_tamanho(hierarquia[nome])
+    hierarquia.setdefault("tamanho", soma_tamanho)
+    return soma_tamanho
+
+
+# Código para Calcular tamanho das de pastas
+def exibir_arvore(hierarquia, nivel=0, profundidade=0, prefixo=""):
+    exibir = ""
+    if profundidade < nivel:
+        return ""
+    for nome, conteudo in hierarquia.items():
+        if nome == "tamanho" or nome == "tipo":
+            continue
+        if conteudo.get("tipo") == "arquivo":
+            icone = "📄"
+        else:
+            icone = "📁"
+        ultimo_item = [
+            chave for chave in hierarquia.keys() if chave not in ["tamanho", "tipo"]
+        ][-1]
+        eh_ultimo = nome == ultimo_item
+        if eh_ultimo:
+            conector = "└── "
+        else:
+            conector = "├── "
+        if eh_ultimo:
+            novo_prefixo = prefixo + "    "
+        else:
+            novo_prefixo = prefixo + "│   "
+
+        tamanho = (
+            (conteudo.get("tamanho")) / (1024**2) if conteudo.get("tamanho") else 0
+        )
+        texto_visual = prefixo + conector + icone + " " + nome
+        exibir += f"{texto_visual:<100}{tamanho:.2f}MB\n"
+        exibir += exibir_arvore(hierarquia[nome], nivel + 1, profundidade, novo_prefixo)
+    return exibir
+
+
+def main(diretorio, profundidade):
+    hierarquia = {}
+    montar_arvore(diretorio, hierarquia)
+    raiz = next(iter(hierarquia))
+    calcular_tamanho(hierarquia[raiz])
+    exibir = exibir_arvore(hierarquia=hierarquia, profundidade=profundidade)
+    print(exibir)
+    return exibir
+
+# diretorio = Path("D:\\dario") / "Documents"
+diretorio = Path.home() / "Documents"
+profundidade = 3
+main(diretorio, profundidade)
